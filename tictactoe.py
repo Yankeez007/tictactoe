2f906d0577f78894c6032388b41d2bbdaf717d70
yassine ouled miled <youledmiled@gmail.com>
2024-06-11 15:18:03 +0200

    crée le plateau de jeu

--- tictactoe.py
+++ tictactoe.py
@@ -1,1 +1,11 @@
 #projet morpion, on va cree un jeu où deux joueurs vont d'affronter sur un plateau 3x3 et le but est de formé une ligne de trois symboles identiques
+
+#on va cree le plateau de jeu
+def initialiser_plateau():
+    plateau = [["","",""],["","",""],["","",""]]
+    return plateau 
+#on va maintenant l'afficher
+def afficher_plateau(plateau):
+    initialiser_plateau()
+    for i in range(3):
+        print(plateau[i])
