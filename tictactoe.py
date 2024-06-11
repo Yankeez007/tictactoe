63a518dc9817f2e32c263143ebd6c795ab82bdba
yassine ouled miled <youledmiled@gmail.com>
2024-06-11 19:56:18 +0200

    determiner le gagnant

--- tictactoe.py
+++ tictactoe.py
@@ -23,6 +23,20 @@ def ajouter_coup(plateau, ligne, colonne, joueur):
     else:
         print("la case n'est pas libre")
 
+#on va faire une fonction qui va verifier le gagnant de la partie donc avec les trois symboles alignés
+def verifier_gagnant(plateau, joueur):
+    for i in range(3):
+        if (plateau[i][0]==joueur and plateau[i][1]==joueur and plateau[i][2]==joueur):
+            return True
+        elif (plateau[0][i]==joueur and plateau[1][i]==joueur and plateau[2][i]==joueur):
+            return True
+        elif (plateau[i][i]==joueur and plateau[i][i]==joueur and plateau[i][i]==joueur):
+            return True
+        else :
+            return False
+    if (plateau[0][2]==joueur and plateau[1][1]==joueur and plateau[2][0]==joueur):
+            return True
+
 plateau = initialiser_plateau()
 afficher_plateau(plateau)
 
