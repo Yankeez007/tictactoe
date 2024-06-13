5de44768d5ad59340acc006b077136560b07549f
yassine ouled miled <youledmiled@gmail.com>
2024-06-13 18:08:11 +0200

    modification de jouer et debut du reajustement de demander_coup()

--- tictactoe.py
+++ tictactoe.py
@@ -54,7 +54,6 @@ def demander_coup(joueur):
     if len(position) != 3 :
         print("coup invalide ! ")
         return -1
-
     ligne = int(position[0]) # 1 en int entre 0 et 2 
     colonne = int(position[2]) # 2 en int entre 0 et 2
     if ligne >= 0 and ligne <= 2 and colonne <= 2 and colonne >= 0 :
@@ -76,27 +75,37 @@ def jouer():
     on s'arrete s'il y a un gagnant ou si le plateau est plein
     """
     plateau = initialiser_plateau()
-    afficher_plateau(plateau)
     joueur1 = "x"
     joueur2 = "o"
+    joueur_actuel = joueur1
+    i = 1 #on crée un indice qui va compter le nombre de tour dans la boucle
     while not est_plein(plateau) or (not verifier_gagnant(plateau, joueur1) or not verifier_gagnant(plateau, joueur2)):
-        coup_j1 = demander_coup(joueur1)
-        ajouter_coup(plateau, coup_j1[0], coup_j1[1], joueur1)
-
-        coup_j2 = demander_coup(joueur2)
-        ajouter_coup(plateau, coup_j2[0], coup_j2[1], joueur2)
-        if est_plein(plateau):
-            return "le plateau est plein, fin de partie, match nul"
-        elif verifier_gagnant(plateau, joueur1):
-            return "le joueur 1 à gagné !"
-        elif verifier_gagnant(plateau, joueur2):
-            return "le joueur 2 à gagné !"
+        afficher_plateau
+        coup = demander_coup(joueur_actuel) #prends la valeur de demander_coup sous la forme d'un couple (ligne,colonne) du joueur actuel
+        if ajouter_coup(plateau, coup[0], coup[1], joueur_actuel) == -1 :
+                print("coup invalide, veuillez réessayer.")
+        else :
+            if verifier_gagnant(plateau, joueur_actuel):
+                afficher_plateau(plateau)
+                return f"Le joueur {joueur_actuel} a gagné !"
+            elif est_plein(plateau):
+                afficher_plateau(plateau)
+                return "Le plateau est plein, fin de parti, match nul"
+            #maintenant il faut passer au joueurs suivant
+            if joueur_actuel == joueur1:
+                joueur_actuel = joueur2 
+            else :
+                joueur_actuel = joueur1
+            i = i + 1
+    afficher_plateau(plateau)
+    return "fin de la partie"
         
-        
 
 
 
 
+
+"""
 plateau = initialiser_plateau()
 afficher_plateau(plateau)
 ajouter_coup(plateau, 0, 2, "X")
@@ -109,6 +118,40 @@ print("est_plein(plateau) = ", est_plein(plateau))
 print("est_plein(plateau_plein) = ", est_plein(plateau_plein))
 #j1="x"
 #demander_coup(j1)
+
+
+
+
+#on met les conditions, si le plateau est plein ou qu'il y a un gagnant, la partie est finie sinon on place les coups
+        elif est_plein(plateau):
+            return "le plateau est plein, fin de partie, match nul"
+        elif verifier_gagnant(plateau, joueur1):
+            return "le joueur 1 à gagné !"
+        elif verifier_gagnant(plateau, joueur2):
+            return "le joueur 2 à gagné !"
+        elif i % 2 == 0 : #idem mais pour le joueur 2
+            if ajouter_coup(plateau, coup_j2[0], coup_j2[1], joueur2) == -1 :
+                print("coup invalide, veuillez réessayer.")
+            else :
+                ajouter_coup(plateau, coup_j2[0], coup_j2[1], joueur2)
+                i = i + 1
+        elif 
+            ajout_coupj1
+            if coup_j1 == -1 :
+                print("veuillez réessayer")
+                ajout_coupj1
+            else  :
+                afficher_plateau(plateau)
+                i = i + 1
+        elif 
+            if coup_j2 == -1 :
+                print("veuillez réessayer")
+                ajout_coupj2
+            else  :
+                afficher_plateau(plateau)
+                i = i + 1
+            """
+
 jouer()
 
 
